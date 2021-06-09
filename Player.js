f183faedf05c4d2f6442c00147413d615a9e59b3
PriyankG85 <84387001+PriyankG85@users.noreply.github.com>
2021-06-09 15:00:01 +0530

    Add files via upload

--- Player.js
+++ Player.js
@@ -1,24 +1,15 @@
-class Player{
-
-    constructor(){
-        this.spt=createSprite(200,100,50,50);
-        this.spt.shapeColor="blue";
-
-    }
-
-    applyGravity(){
-        this.spt.velocityY=this.spt.velocityY+2;
-    }
-    moveRight(){
-        this.spt.x=this.spt.x+12;
-    }
-
-    moveLeft()
+class Player
+{
+    constructor(x, y)
     {
-        this.spt.x=this.spt.x-12;
+        this.x = x;
+        this .y = y;
+        this.spt = createSprite(this.x, this.y, 50, 50);
+        this.spt.shapeColor = "orange";
     }
-
-    jump(){
-        this.spt.velocityY = -33;
+    move(xdir, ydir)
+    {
+        this.spt.x += xdir*grid;
+        this.spt.y += ydir*grid;
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,52 +1,87 @@
-var mario;
-var platformGroup;
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var gameState = "play";
+var carAnimation, logAnimation, playerAnimation;
+var school;
+var player;
 function preload()
 {
  
 }
 
 function setup() {
-  createCanvas(displayWidth, 668);
-  
-
-  var countDistanceX = 0;
-  var platform;
-  var gap;
- 
-
-  mario=new Player();
-  //console.log(countDistanceX);
-  platformGroup=createGroup();
-  for (var i=0;i<14;i++)
-	 {
-      platform = new Platform(countDistanceX);
-      platformGroup.add(platform.spt);
-     
-      gap=random([0,0,0,0,80]);
-      countDistanceX = countDistanceX + platform.rw + gap; 
-      //console.log(countDistanceX);
-     }
-  
+  createCanvas(1366,700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
 }
 
 function draw() {
-  background('skyblue');  
-  translate(-mario.spt.x+100, 0);
-  mario.applyGravity();
-  mario.spt.collide(platformGroup);
-
-  if(keyDown("right")){
-    mario.moveRight();
+  background("skyblue");
+  // Grasses
+  for(var i=0; i<6; i++)
+  {
+    var bttomGrass1 = createSprite(683, height-50-(i*400), width, grassHeight);
+    if (i%2===0)
+    {
+      var road = createSprite(683, height-150-(i*400), width, 300,)
+      road.shapeColor = "black";
+    }
+    bttomGrass1.shapeColor = "grey";
   }
-  if(keyDown("left")){
-    mario.moveLeft();
+  for(var i=0; i<40; i++)
+  {
+    cars = new Car(2);
+    carGroup1.add(cars.spt);
+    logs = new Log(-2);
+    logGroup1.add(logs.spt);
+  }
+  //Making logs reappear
+  for(i=1;i<logGroup1.length;i++)
+  {
+    if (logGroup1[i].x<0)
+    {
+      logGroup1[i].x = width;
     }
-
-    if(keyDown("up")){
-      mario.jump();
+  }
+  // Making cars reappear
+  for(i=1;i<carGroup1.length;i++)
+  {
+    if (carGroup1[i].x<0)
+    {
+      carGroup1[i].x = width;
+      carGroup1[i].x = -(width);
     }
+  }
 
-
+  // Creating Player
+  player = new Player(width/2, height-25);
   
+  // Function to move the player with the help of arrow keys
+  keyPressed();
+
+  translate(0, -player.spt.y+height-150);
+
   drawSprites();
 }
+function keyPressed()
+{
+  if (keyCode == UP_ARROW)
+  {
+    player.move(0, -2);
+  }
+  else if (keyCode == DOWN_ARROW)
+  {
+    player.move(0, 2);
+  }
+  else if (keyCode == LEFT_ARROW)
+  {
+    player.move(-2, 0);
+  }
+  else if (keyCode == RIGHT_ARROW)
+  {
+    player.move(2, 0);
+  }
+}
+
